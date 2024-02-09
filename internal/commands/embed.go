b173045eb404aeecda503c0573c27307d38126a3
Eli Bendersky <eliben@gmail.com>
2024-02-09 06:41:14 -0800

    Support output formats for embeddings

--- internal/commands/embed.go
+++ internal/commands/embed.go
@@ -1,9 +1,14 @@
 package commands
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/eliben/gemini-cli/internal/apikey"
 	"github.com/google/generative-ai-go/genai"
@@ -28,6 +33,7 @@ func init() {
 
 	embedCmd.Flags().StringP("content", "c", "", "content to embed")
 	embedCmd.Flags().StringP("model", "m", "embedding-001", "name of embedding model to use")
+	embedCmd.Flags().StringP("format", "", "float", "format for embedding output: float, base64, blob")
 }
 
 func runEmbedCmd(cmd *cobra.Command, args []string) {
@@ -46,8 +52,59 @@ func runEmbedCmd(cmd *cobra.Command, args []string) {
 	res, err := model.EmbedContent(ctx, genai.Text(content))
 
 	if emb := res.Embedding; emb != nil {
-		fmt.Println(emb.Values)
+		format, _ := cmd.Flags().GetString("format")
+		emitEmbedding(os.Stdout, emb.Values, format)
 	} else {
 		log.Fatal("got no embeddinb back from model")
 	}
+}
+
+func emitEmbedding(w io.Writer, v []float32, format string) {
+	switch format {
+	case "float":
+		fmt.Fprintln(w, v)
+	case "base64":
+		b := encodeEmbedding(v)
+		encoder := base64.NewEncoder(base64.StdEncoding, w)
+		encoder.Write(b)
+		encoder.Close()
+		fmt.Println()
+	case "blob":
+		b := encodeEmbedding(v)
+		w.Write(b)
+	default:
+		log.Fatalf("invalid format: %s", format)
+	}
+}
+
+// encodeEmbedding encodes an embedding into a byte buffer, e.g. for DB
+// storage as a blob.
+func encodeEmbedding(emb []float32) []byte {
+	buf := new(bytes.Buffer)
+	for _, f := range emb {
+		err := binary.Write(buf, binary.LittleEndian, f)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return buf.Bytes()
+}
+
+// decodeEmbedding decodes an embedding back from a byte buffer.
+func decodeEmbedding(b []byte) []float32 {
+	buf := bytes.NewReader(b)
+
+	// Calculate how many float32 values are in the slice
+	count := buf.Len() / 4
+	numbers := make([]float32, 0, count)
+
+	for i := 0; i < count; i++ {
+		var num float32
+		err := binary.Read(buf, binary.LittleEndian, &num)
+		if err != nil {
+			panic(err)
+		}
+		numbers = append(numbers, num)
+	}
+	return numbers
 }
